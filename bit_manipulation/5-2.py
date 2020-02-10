cf02436f7ea0a8793184799e86f482b70299a76e
MasahiroKitazoe <sas.surfer0jonny@gmail.com>
2020-02-10 20:08:08 +0900

    Update 5-2.py

--- bit_manipulation/5-2.py
+++ bit_manipulation/5-2.py
@@ -3,7 +3,9 @@ import unittest
 
 def binary_to_string(float_val):
     binary_representation = "0."
-    while len(binary_representation) <= 32 and float_val > 0:
+    while len(binary_representation) <= 32:
+        if float_val == 0:
+            return binary_representation
         multiplied = float_val * 2
         binary_representation += str(int(multiplied))
         float_val = multiplied - int(multiplied)
@@ -13,6 +15,7 @@ def binary_to_string(float_val):
 class Test2(unittest.TestCase):
     def test_it(self):
         assert binary_to_string(float_val=0.72) == "ERROR"
+        assert binary_to_string(float_val=0.5) == "0.1"
 
 
 if __name__ == "__main__":
